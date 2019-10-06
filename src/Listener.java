79b9f6d4f977ce2f91b8f7ee938fa3c3215f3bc6
JadeWang96 <wangyujue19960222@gmail.com>
2019-10-06 16:24:27 -0400

    multithreaded udp for each site

--- src/Listener.java
+++ src/Listener.java
@@ -1,63 +1,53 @@
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 /*
 The implementation of UDP listener
-Listen the msgs from other sites
+Listen the msgs from a specific site
  */
 public class Listener extends Thread {
     private DatagramSocket socket;
+    private String listenTo;// the site ID this socket listens to
     private boolean running;
     private byte[] buffer = new byte[65535];
 
-    public Listener(DatagramSocket updSocket,  ArrayList<HashMap<String, String>> sitesInfo, ReservationSys mySite) {
+    public Listener(DatagramSocket updSocket,  ArrayList<HashMap<String, String>> sitesInfo, ReservationSys mySite, String listenTo) {
         this.socket = updSocket;
+        this.listenTo = listenTo;
         running = false;
     }
 
-    public void run() {
+    public void run(ArrayList<HashMap<String, String>> sitesInfo, ReservationSys mySite) throws IOException, ClassNotFoundException {
         running = true;
-
+        DatagramPacket packet = null;
         while (running) {
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            packet = new DatagramPacket(buffer, buffer.length);
             try {
                 socket.receive(packet);// blocks until a msg arrives
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            InetAddress address = packet.getAddress();
-            int port = packet.getPort();
-            packet = new DatagramPacket(buffer, buffer.length, address, port);
-            // get the new class
+            System.out.println("Got something from site " + listenTo);
+            // Update the current site based on the received information
+            mySite.update((CommunicateInfo) deserialize(packet.getData()), listenTo);
+            // TODO: send received confirmation?
 
+            buffer = new byte[65535];//reset
         }
         socket.close();
     }
 
 
     // Deserialize the byte array and reconstruct the object
-    /*
-    public deserialize() {
-        ByteArrayInputStream bis = new ByteArrayInputStream(this.buffer);
-        ObjectInput in = null;
-        try {
-            in = new ObjectInputStream(bis);
-            Object o = in.readObject();
-
-        } finally {
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (IOException ex) {
-                // ignore close exception
-            }
-        }
+    public static Object deserialize(byte[] buffer) throws IOException, ClassNotFoundException{
+        ByteArrayInputStream byteStream = new ByteArrayInputStream(buffer);
+        ObjectInputStream objStream = new ObjectInputStream(byteStream);
+        return objStream.readObject();
     }
-    */
 }
